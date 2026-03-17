9ae0721bc45ae2dbcc67e145d56bf509a77263fa
Dávisson Falcão <davissonbeat@hotmail.com>
2026-03-17 17:22:03 -0300

    feat: modernização da header e habilitcação da página do admin

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  ngOnInit() {
+    window.addEventListener('scroll', () => {
+      const navbar = document.querySelector('.custom-navbar');
+      if (window.scrollY > 50) {
+        navbar?.classList.add('scrolled');
+      } else {
+        navbar?.classList.remove('scrolled');
+      }
+    });
+  }
+}
